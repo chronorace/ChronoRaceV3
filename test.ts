2b085884e5c14d989e2022ec0e4c85c13d25f0c0
christianlemasson <christian.lemasson@chronorace.be>
2014-07-25 09:21:40 +0200

    un nouveau commit depuis notepad

--- test.ts
+++ test.ts
@@ -5,8 +5,9 @@ module cr.test{
     class zen{
         constructor(private name?:string){
             if (!name) this.name="cle";
-	alert('cool... c est la merde?');
+		alert('cool... c est la merde?');
             // test conflit depuis notepad
+		// encore un ajout dans notepad a recuperer ds webstorm
         }
     }
 }
